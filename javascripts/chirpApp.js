d991e44cc92f487a6c8042bea73340d956d37915
John Park <jwpark417@gmail.com>
2016-06-24 15:58:58 -0400

    Add post/posts/newPost in mainController

--- javascripts/chirpApp.js
+++ javascripts/chirpApp.js
@@ -1,5 +1,19 @@
 var app = angular.module('chirpApp', []);
 
 app.controller('mainController', function($scope) {
+  var $scope.posts = [];
+  var $scope.newPost = {
+    created_by: '',
+    text: '',
+    created_at: ''
+  };
 
+  $scope.post = function() {
+    $scope.newPost.created_at = Date.now();
+    $scope.posts.push($scope.newPost);
+    $scope.newPost = {
+      created_by: '',
+      text: '',
+      created_at: ''
+  };
 });
