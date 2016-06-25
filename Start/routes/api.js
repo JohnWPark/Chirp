2c3c13c1c24845e4021118f7b3759d4c07601766
John Park <jwpark417@gmail.com>
2016-06-25 15:01:18 -0400

    Start express server

---
+++ Start/routes/api.js
@@ -0,0 +1,18 @@
+var express = require('express');
+var router = express.Router();
+
+router.route('/posts')
+
+  //returns all posts
+  .get(function(req, res){
+
+    //temp solution
+    res.send({message: 'TODO return all posts'});
+  })
+
+  .post(function(req, res){
+    //temp solution
+    res.send({message: 'TODO Create a new post'});
+  });
+
+module.exports = router;
